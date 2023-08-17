360ee0023febaaf2b8dde478f2ce16c219e86a3c
Pride Leong <lykling.lyk@gmail.com>
2023-08-17 20:37:42 +0800

    chore: add functions for modify bazel BUILD file
    Signed-off-by: Pride Leong <lykling.lyk@gmail.com>

--- irun.sh
+++ irun.sh
@@ -17,7 +17,11 @@
 # Flags
 #set -u
 #set -e
-readonly SCRIPT_DIR=$(cd $(dirname $0); pwd)
+#set -x
+readonly SCRIPT_DIR=$(
+  cd $(dirname $0)
+  pwd
+)
 
 ###############################################################################
 # Breif:
@@ -29,15 +33,15 @@ readonly SCRIPT_DIR=$(cd $(dirname $0); pwd)
 #   0: run command succeed
 #   ?: run command failed with exit code $?
 ###############################################################################
-function run() {
-    local cmd=$1
-    shift
-    ${cmd} "$@"
-    local ret=$?
-    [[ "$ret" != "0" ]] && \
-        err "command error $ret: ${cmd} $@, exiting" && \
-        exit $ret
-    return 0
+run() {
+  local cmd=$1
+  shift
+  ${cmd} "$@"
+  local ret=$?
+  [[ "$ret" != "0" ]] &&
+    err "command error $ret: ${cmd} $@, exiting" &&
+    return $ret
+  return 0
 }
 
 ###############################################################################
@@ -48,8 +52,8 @@ function run() {
 # Returns:
 #   None
 ###############################################################################
-function err() {
-    echo "[ERROR] $(date +"%Y-%m-%d %H:%M:%S") $@" >&2
+err() {
+  echo "[ERROR] $(date +"%Y-%m-%d %H:%M:%S") $@" >&2
 }
 
 ###############################################################################
@@ -60,27 +64,27 @@ function err() {
 # Returns:
 #   None
 ###############################################################################
-function info() {
-    echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") $@" >&1
+info() {
+  echo "[INFO] $(date +"%Y-%m-%d %H:%M:%S") $@" >&1
 }
 
-function join_by() {
-    local IFS="$1"
-    shift
-    echo "$*"
+join_by() {
+  local IFS="$1"
+  shift
+  echo "$*"
 }
 
-function _ii_usage {
-    echo "Usage:
+_ii_usage() {
+  echo "Usage:
     $0 <func> [...args]
 Examples:
     "
 }
 
-function _ii_create_dir_index_html {
-    local dir="$(readlink -f $1)"
-    local out="${2:-$dir}"
-    find "${dir}" -type d -exec bash -c "\
+_ii_create_dir_index_html() {
+  local dir="$(readlink -f $1)"
+  local out="${2:-$dir}"
+  find "${dir}" -type d -exec bash -c "\
         dir=$dir; \
         out=$out; \
         source=\${0}; \
@@ -89,7 +93,6 @@ function _ii_create_dir_index_html {
         mkdir -p \${target}; \
         tree \${source} \
         -H "." \
-        -L 1 \
         --noreport \
         --dirsfirst \
         --charset utf-8 \
@@ -98,6 +101,8 @@ function _ii_create_dir_index_html {
         -I index.html \
         -T Files \
         -s \
+        -h \
+        --du \
         -D \
         -F \
         -C \
@@ -105,31 +110,103 @@ function _ii_create_dir_index_html {
         " {} \;
 }
 
-function _ii_edeps {
-    local pkg=$1
-    # qdepends -CQqqF'%{CAT}/%{PN}:%{SLOT}' "^${pkg}" | tr '\n' ' '
-    qdepends -CQqqF'%{CAT}/%{PN}:%{SLOT}' "^${pkg}"
+_ii_edeps() {
+  local pkg=$1
+  # qdepends -CQqqF'%{CAT}/%{PN}:%{SLOT}' "^${pkg}" | tr '\n' ' '
+  qdepends -CQqqF'%{CAT}/%{PN}:%{SLOT}' "^${pkg}"
 }
 
-function main {
-    if [[ ( -L "$0" ) && ( "$(type -t _ii_$(basename $0))" == "function" ) ]]; then
-        # symlink alias, use filename as command
-        cmd="$(basename $0)"
-    else
-        cmd=$1
-        if [[ "$#" > 0 ]]; then
-            shift
-        fi
+_ii_replace_apollo_cc_library() {
+  files=("$@")
+  current_pkg=""
+  for x in $(cat "${files[@]}" | tr -d '",' | sort); do
+    pkg=${x%:*}
+    slot=${x#*:}
+    if [[ "${slot}" =~ .*proto ]]; then
+      echo "skip proto target ${x}"
+      continue
     fi
-    if [[ "${cmd}" == "" ]]; then
-        cmd="usage"
+    if buildozer "print srcs" ${x} |& grep -c "\.so" 2>&1 > /dev/null; then
+      echo "skip shared library target ${x}"
+      continue
     fi
-    run _ii_${cmd} "$@"
+    if [[ "${pkg}" != "${current_pkg}" ]]; then
+      current_pkg="${pkg}"
+      buildozer "new_load //tools:apollo_package.bzl apollo_cc_library" "${pkg}:__pkg__"
+      echo "add new load of apollo_cc_library for ${pkg}"
+    fi
+    buildozer "set kind apollo_cc_library" "${x}"
+    echo "process ${x} done"
+  done
+}
+
+_ii_find_apollo_package_paths() {
+  for x in $(find cyber modules -name cyberfile.xml); do
+    echo $(dirname $x)
+  done
+}
+
+_ii_get_external_dep_targets() {
+  package_path=$1
+  for target in $(buildozer "print label" ${package_path}/...:%cc_library); do
+    for dep in $(buildozer "print deps" ${target} 2> /dev/null | grep -oP '(?<=[\[ "])//.*?(?=[ "\]])'); do
+      if [[ "${dep}" =~ ^//${package_path}[/:] ]]; then
+        :
+      else
+        echo ${dep}
+      fi
+    done
+  done | sort | uniq | grep -v third_party | grep -v -E 'proto$'
+}
+
+_ii_fix_so_lib_to_apollo_cc_library() {
+  target=$1
+  if [[ "${target}" =~ .*:.* ]]; then
+    pkg=${target%:*}
+    slot=${target#*:}
+  else
+    pkg=${target}
+    slot="${target##*/}"
+  fi
+  buildozer "new_load //tools:apollo_package.bzl apollo_cc_library" "${pkg}:__pkg__"
+  # bin_rule=$(buildozer "print srcs" ${target} | tr -d '[]:')
+  bin_rule=$(buildozer "print srcs" ${target} | tr -d '[]:')
+
+  buildozer "new apollo_cc_library ${slot}_tmp before ${slot}" "${pkg}:__pkg__"
+  buildozer "copy hdrs ${slot}" "${pkg}:${slot}_tmp"
+  buildozer "set srcs $(buildozer "print srcs" ${pkg}:${bin_rule} | tr -d '[]' | tr ' ' '\n' | grep -o -E '.*.cc')" "${pkg}:${slot}_tmp"
+
+  bin_deps=$(buildozer "print deps" ${pkg}:${bin_rule} | tr -d '[]' | grep -v missing)
+  lib_deps=$(buildozer "print deps" ${pkg}:${slot} | tr -d '[]' | grep -v missing)
+  buildozer "set deps ${bin_deps} ${lib_deps}" "${pkg}:${slot}_tmp"
+
+  buildozer "delete" "${pkg}:${slot}"
+  buildozer "delete" "${pkg}:${bin_rule}"
+
+  buildozer "set name ${slot}" "${pkg}:${slot}_tmp"
+
+  echo "fix ${target} done"
+}
+
+main() {
+  if [[ (-L "$0") && ("$(type -t _ii_$(basename $0))" == "function") ]]; then
+    # symlink alias, use filename as command
+    cmd="$(basename $0)"
+  else
+    cmd=$1
+    if [[ "$#" > 0 ]]; then
+      shift
+    fi
+  fi
+  if [[ "${cmd}" == "" ]]; then
+    cmd="usage"
+  fi
+  run _ii_${cmd} "$@"
 }
 
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
-    # Being sourced, do nothing
-    :
+  # Being sourced, do nothing
+  :
 else
-    main "$@"
+  main "$@"
 fi
